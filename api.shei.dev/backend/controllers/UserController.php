9cd08ce8cc4148b520380d20ca1f3d2fa9c604ce
Kiarash Mozafari کیارش مظفری <kiamoz.gtalk@gmail.com>
2025-09-23 20:22:55 +0330

    save user + toast

---
+++ api.shei.dev/backend/controllers/UserController.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace backend\controllers;
+
+use common\models\User;
+use common\models\User_s;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use Yii;
+
+/**
+ * UserController implements the CRUD actions for User model.
+ */
+class UserController extends Controller
+{
+        public $enableCsrfValidation = false;
+
+    /**
+     * @inheritDoc 
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all User models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $searchModel = new User_s();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single User model.
+     * @param int $id
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new User model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new User();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing User model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing User model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the User model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id
+     * @return User the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = User::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+    
+    public function actionLogin(){
+        
+       
+        $clean_username = filter_input(INPUT_POST, 'user_email', FILTER_SANITIZE_EMAIL);
+        $found_user = User::find()->where(['username'=>$clean_username])->One();
+        if($found_user){
+            // login
+           return json_encode(['err'=>'password is wrong']);
+        }else{
+            // signup
+            $shei = new User();
+            $shei->username = $clean_username;
+            $shei->email = $clean_username; 
+            
+            $shei->auth_key = Yii::$app->security->generateRandomString();
+            $shei->password_hash = Yii::$app->security->generatePasswordHash($_POST['user_password']);
+            $shei->status = 10;
+            if($shei->save()){
+                return json_encode(['ok'=>'yes','username'=>$shei->username]);
+            }else{
+                return json_encode(['err'=>print_r($shei->getErrors(),1)]);
+            }
+             
+
+            
+        }
+
+        
+    }
+}
--- api.shei.dev/backend/web/index.php
+++ api.shei.dev/backend/web/index.php
@@ -1,7 +1,21 @@
 <?php
 
+header('Access-Control-Allow-Origin:*');
+
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods:*');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("HTTP/1.1 200 OK");
+    die();
+}
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+date_default_timezone_set('Asia/Tehran');
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
